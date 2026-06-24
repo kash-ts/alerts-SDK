b79bce045aeb129553e96286352bc1d7145c4127
Mikhail <ipomainkra@gmail.com>
2026-06-25 09:44:49 +1000

    feat(exports): update public API exports for v1.3.0

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,38 @@
 import getAuthorizeLink from "@function/getAuthorizeLink.js";
 import getOauthToken from "@function/getOauthToken.js";
 import getUser from "@function/getUser.js";
-import updateAccessToken from "@function/updateAccessToken.js";
+import updateOauthToken from "@function/updateOauthToken.js";
 import getUserChannel from "@function/getUserChannel.js";
-import { getPrivateToken } from "@function/getPrivateToken.js";
-import CentrifugeClient from "@ws/CentrifugeClient.js";
+import getPrivateToken from "@function/getPrivateToken.js";
+import getDonationsAlerts from "@function/getDonationsAlerts.js";
+import getExternal from "@function/getExternal.js";
+import createCustomAlerts from "@function/createCustomAlerts.js";
+import generateSignature from "@function/generateSignature.js";
+import createMerchandise from "@function/createMerchandise.js";
+import updateMerchandise from "@function/updateMerchandise.js";
+import updateOrCreateMerchandise from "@function/updateOrCreateMerchandise.js";
+import getUserDataFromPromocode from "@function/getUserDataFromPromocode.js";
+import sendSaleAlert from "@function/sendSaleAlert.js";
 
-export * from "@type";
+import WebServer from "@ws/CentrifugeClient.js";
+import { OAuthScope } from "@type";
 
 export {
     getAuthorizeLink,
     getOauthToken,
     getUser,
-    updateAccessToken,
+    updateOauthToken,
     getUserChannel,
+    getExternal,
     getPrivateToken,
-    CentrifugeClient
+    getDonationsAlerts,
+    createCustomAlerts,
+    generateSignature,
+    createMerchandise,
+    updateMerchandise,
+    updateOrCreateMerchandise,
+    getUserDataFromPromocode,
+    sendSaleAlert,
+    WebServer,
+    OAuthScope
 }; 
